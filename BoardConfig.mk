9efa3018a613e5313db402b73e32390dd1c438f1
rogersb11 <brettrogers11@gmail.com>
2015-01-17 19:18:24 -0500

    Allow flashing for d2tmobile as well so save us time

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@
 -include vendor/samsung/d2att/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2lte
+TARGET_OTA_ASSERT_DEVICE := d2att,d2lte,d2tmo
 
 TARGET_BOARD_INFO_FILE ?= device/samsung/d2att/board-info.txt
 
